2ab04ccb9c18594bdbdf4f8ee0a014d3b5739b14
Keertana <gubbala3@msu.edu>
2024-09-28 21:01:16 -0400

    scroll and search feature

--- MaizePlanner/mern/client/src/components/ScheduleBuilder.jsx
+++ MaizePlanner/mern/client/src/components/ScheduleBuilder.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Select from "react-select";
 import "./ScheduleBuilder.css"; 
+
+const courses = [
+  { value: "class1", label: "Class 1" },
+  { value: "class2", label: "Class 2" },
+  { value: "class3", label: "Class 3" },
+  { value: "class4", label: "Class 4" },
+  { value: "class5", label: "Class 5" },
+  { value: "class6", label: "Class 6" },
+  { value: "class7", label: "Class 7" },
+  { value: "class8", label: "Class 8" },
+  { value: "class9", label: "Class 9" },
+  { value: "class10", label: "Class 10" },
+];
+
+const courseCodes = [
+  { value: "code1", label: "Code 1" },
+  { value: "code2", label: "Code 2" },
+  { value: "code3", label: "Code 3" },
+  { value: "code4", label: "Code 4" },
+  { value: "code5", label: "Code 5" },
+  { value: "code6", label: "Code 6" },
+  { value: "code7", label: "Code 7" },
+  { value: "code8", label: "Code 8" },
+  { value: "code9", label: "Code 9" },
+  { value: "code10", label: "Code 10" }
+];
 
 const ScheduleBuilder = () => {
   return (
@@ -7,24 +34,34 @@ const ScheduleBuilder = () => {
       {/* First Section: Semester */}
       <div className="section">
         <h1>SEMESTER</h1>
-        <select className="dropdown">
-          <option value="">Choose a semester</option>
-          <option value="Spring 2024">Spring 2024</option>
-          <option value="Summer 2024">Summer 2024</option>
-          <option value="Fall 2024">Fall 2024</option>
-        </select>
+        <Select
+          className="dropdown-select"
+          options={[
+            { value: "Spring 2024", label: "Spring 2024" },
+            { value: "Summer 2024", label: "Summer 2024" },
+            { value: "Fall 2024", label: "Fall 2024" }
+          ]}
+          placeholder="Choose a semester"
+        />
       </div>
 
       {/* Second Section: Classes */}
       <div className="section">
         <h1>CLASSES</h1>
-        <select className="dropdown">
-          <option value="">Select a class</option>
-          {/* These options will later be populated from the database */}
-          <option value="class1">Class 1</option>
-          <option value="class2">Class 2</option>
-          <option value="class3">Class 3</option>
-        </select>
+        <div className="dropdown-container">
+          <Select
+            className="dropdown-select"
+            options={courses}
+            placeholder="Course name"
+            isSearchable
+          />
+          <Select
+            className="dropdown-select"
+            options={courseCodes} 
+            placeholder="Course code"
+            isSearchable
+          />
+        </div>
       </div>
 
       {/* Third Section: Your Schedule */}
